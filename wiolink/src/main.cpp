e6117e41e91f1798fd7cd7e0d68d0ea441e34b9e
Alex Korovyansky <alexkorovyansky@gmail.com>
2016-11-20 14:51:06 +0700

    Task 2

--- wiolink/src/main.cpp
+++ wiolink/src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
+#include <Grove_LED_Bar.h>
+
+Grove_LED_Bar led_bar(13, 12, 0);
+int level = 10;
 
 void setup() {
-    // инициализация Serial порта c baudrate равным 28800
     Serial.begin(28800);
-
-    // Для включения GPIO портов на плате WioLink необходимо подать высокий сигнал на 15-ый пин микропроцессора.
     pinMode(15, OUTPUT);
     digitalWrite(15, HIGH);
+
+    led_bar.begin();
 }
 
 void loop() {
-    Serial.println("Hello World! Devfest Siberia 2016 rulezzzz");
-    delay(1000);
+  led_bar.setLevel(level);
+  if (level > 0) {
+      level = level - 1;
+  }
+  delay(1000);
 }
